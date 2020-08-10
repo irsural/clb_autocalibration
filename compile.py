d6aafd0de1f309a8d98eed304806a6fc72977b21
feelinglight <kolpacc123@mail.ru>
2020-08-10 11:09:59 +0500

    Поправил скрипт сборки

--- compile.py
+++ compile.py
@@ -14,15 +14,15 @@ def restore_convert_ui_strings(a_tmp_file):
 
 
 if __name__ == "__main__":
-    tmp_file = "main_to_compile.py"
+    tmp_file = "Clb_AutoCalibration.py"
 
     delete_convert_ui_strings(tmp_file)
     os.system(
-        f'pyinstaller -n AutoCalibration --onefile --noconsole --icon=main_icon.ico '
+        f'pyinstaller --noconsole --onefile --icon=main_icon.ico '
         f'--add-data "C:\\Windows\\System32\\vcruntime140d.dll";. '
         f'--add-data "C:\\Windows\\System32\\ucrtbased.dll";. '
-        f'--add-data "C:\\Users\\503\\Desktop\\Python projects\\autocalibration\\irspy\\clb\\clb_driver_dll.dll";. '
-        f'--add-data "C:\\Users\\503\\Desktop\\Python projects\\autocalibration\\irspy\\dlls\\mxsrclib_dll.dll";. '
+        f'--add-data "C:\\Users\\503\\Desktop\\Python projects\\clb_autocalibration\\irspy\\clb\\clb_driver_dll.dll";. '
+        f'--add-data "C:\\Users\\503\\Desktop\\Python projects\\clb_autocalibration\\irspy\\dlls\\mxsrclib_dll.dll";. '
         f'{tmp_file}'
     )
     restore_convert_ui_strings(tmp_file)
--- revisions.py
+++ revisions.py
@@ -2,5 +2,5 @@ from enum import IntEnum
 
 
 class Revisions(IntEnum):
-    clb_autocalibration = 161
+    clb_autocalibration = 162
     irspy = 34
